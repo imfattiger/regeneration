d9a0d2b0698a8b7815d2d066183aef32d4f27252
imfattiger <imfattiger@github.com>
2026-04-11 15:14:03 +0800

    Fix LinesGrid: use locale-aware Link from i18n/navigation

--- components/home/LinesGrid.tsx
+++ components/home/LinesGrid.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
 import { useLocale } from "next-intl";
+import { Link } from "@/i18n/navigation";
 
 const LINES = [
   {
